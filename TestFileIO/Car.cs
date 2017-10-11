21c23d62bddb3b56d33a4b59a3da9301cfb0fd94
trentonsweat <trentonsweat@gmail.com>
2017-10-11 14:01:16 -0400

    first commit

---
+++ TestFileIO/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TestFileIO
+{
+    class Car
+    {
+        protected string make;
+        protected string model;
+        protected int year;
+        protected double price;
+        public string Make
+        {
+            set { make = value; }
+            get { return make; }
+        }
+        public string Model
+        {
+            set { model = value; }
+            get { return model; }
+        }
+        public double Price
+        {
+            set { price = value; }
+            get { return price; }
+        }
+        public int Year
+        {
+            set { year = value; }
+            get { return year; }
+        }
+        public Car()
+        {
+            make = " ";
+            model = " ";
+            year = 0;
+            price = 0;
+        }
+        public Car(string car_make, string car_model, int car_year, double car_price)
+        {
+            make = car_make;
+            model = car_model;
+            year = car_year;
+            price = car_price;
+        }
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine(Make + " " + Model + " " + Year + " " + Price);
+        }
+        public override string ToString()
+        {
+            return (Make + " " + Model + " " + Year + " " + Price);
+        }
+    }
+}
---
+++ TestFileIO/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // write to a file
+            //StreamWriter wr = new StreamWriter("Cars.txt", true); // sends a stream of characters to a file
+
+            //wr.WriteLine("Jeep\tCompass\t2015\t30000"); // instead of printing to console prints to file
+            //wr.WriteLine("Chevy\tCruze\t2017\t20000");
+
+            //wr.Close();
+
+            //append
+
+            // read from a file
+
+            StreamReader rd = new StreamReader("Cars.txt");
+
+            // string str = rd.ReadToEnd(); // reads the contents of the whole file
+
+            // Console.WriteLine(str);
+
+            List<Car> CarList = new List<Car>();
+
+            string str = rd.ReadLine(); // this will read only one line
+
+            while (str != null) // while we still have more lines to read
+            {
+                // Console.WriteLine(str); // print the current line
+                
+
+                string[] tempCar = str.Split(); //  an array to split the line you just read into four pieces
+
+                CarList.Add(new Car(tempCar[0], tempCar[1], int.Parse(tempCar[2]), double.Parse(tempCar[3]))); // makes a new car to store all information you just read
+
+                str = rd.ReadLine(); // read the next line (fetch a new one)
+            }
+
+            foreach (Car Element in CarList)
+            {
+                Console.WriteLine(Element.ToString());
+            }
+
+
+
+        }
+
+
+    }
+
+
+}
